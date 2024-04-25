84363a68fe1292b25e79919121d841de064bd422
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-04-25 08:29:26 +0000

    update the code

--- example1.py
+++ example1.py
@@ -2,3 +2,19 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 sum = num1 + num2
 print("The sum of the two numbers is", sum)
+
+
+# IF ELSE STATEMENT 
+
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+
+elif age == 16:
+    print("You will be eligible to vote next 2 year.")
+
+elif age == 17:
+    print("You will be eligible to vote next year.")
+
+else:
+    print("You are not eligible to vote.")
